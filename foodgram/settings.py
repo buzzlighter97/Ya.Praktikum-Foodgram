b8da3693bf1b7f66ae35364d00a9513a794e0265
Ivan Ivanov <buzzlighter97@gmail.com>
2021-03-28 22:46:26 +0300

    commit

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -11,7 +11,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = '3n5v%_kz!mj3l4o&82xy)zn27-qe43wy3zjw95yt8omik8g+-+'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '178.154.194.182',
