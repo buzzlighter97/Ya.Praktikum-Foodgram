c57c077294a46460c17873b9bbcc4d6861e66a0b
Ivan Ivanov <buzzlighter97@gmail.com>
2021-04-01 16:28:35 +0300

    commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ sleep 10
 python manage.py migrate
 python manage.py createcachetable
 python manage.py collectstatic  --noinput
-docker-compose run web python manage.py loaddata fixtures.json
 gunicorn foodgram.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"
