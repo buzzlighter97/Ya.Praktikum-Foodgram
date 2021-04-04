5431c59379b4e7bc445e9a3f580a54d429f80b5a
Ivan Ivanov <buzzlighter97@gmail.com>
2021-04-04 10:40:53 +0300

    commit

--- recipes/validators.py
+++ recipes/validators.py
@@ -7,11 +7,12 @@ def validate_file_size(image):
     if filesize > 1000000:
         raise ValidationError(
             message=(
-                'Максимальный размер изображения не должен превышать  1 Мбайт'
+                'Максимальный размер изображения не должен превышать 1 Мбайт'
             )
         )
     else:
         return image
+
 
 def validate_not_empty(value):
     if value == '':
